83352b95c64268be39c3596895ef67735731ac17
Jeison Cordoba <jeisonsmit@gmail.com>
2025-10-06 08:47:54 -0500

    Create Console, Class Insert at Beginning and Insert at End.

---
+++ LinkedList/LinkedList.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using LinkedList.Cor;
+
+var list = new SinglyLinkedList<string>();
+list.InsertAtBeginning("Perro");
+list.InsertAtBeginning("Gato");
+list.InsertAtBeginning("Cabra");
+list.InsertAtBeginning("Vaca");
+list.InsertAtEnd("Tigre");
+list.InsertAtEnd("Hiena");
+list.InsertAtEnd("Ñu");
+list.InsertAtEnd("Cocodrilo");
+
+list.PrintList();
---
+++ LinkedList/LinkedList.Cor/SimpleNode.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkedList.Cor;
+
+public class SimpleNode<T>
+{    
+    public SimpleNode(T data) 
+    {
+        Data = data;
+        Next = null;
+    }
+    //Constructor and properties
+    public T? Data { get; set; }
+    public SimpleNode<T>? Next { get; set; }//Pointer
+}
